fc3b40a41869e684522cb415cc27a6084e8215ba
Jerry Wang <jerryappleid761208@gmail.com>
2019-07-13 15:39:50 -0700

    first

---
+++ src/app.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import * as uuid from 'uuid/v1';
+
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './create-task.dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+}
